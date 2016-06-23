53dfbd0e3ed45beb3f8ec9cbd61a1e93bff75246
小杨 <yangsea@aliyun.com>
2016-06-23 18:27:43 +0800

    dao

--- blog/src/main/java/com/ocean/frame/main/dao/Impl/ArticleDaoImpl.java
+++ blog/src/main/java/com/ocean/frame/main/dao/Impl/ArticleDaoImpl.java
@@ -2,6 +2,8 @@ package com.ocean.frame.main.dao.Impl;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
 //import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -17,8 +19,16 @@ public class ArticleDaoImpl extends BaseHibernateTemplate implements ArticleDao 
     @Override
     public List<Article> articleList(Article article) {
         
-        List<Article> articleList = this.getHibernateTemplate().find(" From Article ");
-        return articleList;
+//        List<Article> articleList = this.getHibernateTemplate().find(" From Article ");
+        // use ehcache ,necessary sessionfactory model
+        Session session = this.getHibernateTemplate().getSessionFactory().getCurrentSession();
+        Query query = session.createQuery("from Article");
+        query.setCacheable(true);
+        List<Article> list1 = query.list();
+        System.out.println(list1.size());
+        List<Article> list2 = query.list();
+        System.out.println(list2.size());
+        return list1;
     }
 
     @Override
