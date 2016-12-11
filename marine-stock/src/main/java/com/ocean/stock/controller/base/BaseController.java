00f26746ec2ec670803c1af9f9a8f5ebb84ab0b4
oceanyang520 <yangsea@aliyun.com>
2016-12-12 00:19:41 +0800

    pages

---
+++ marine-stock/src/main/java/com/ocean/stock/controller/base/BaseController.java
@@ -0,0 +1,5 @@
+package com.ocean.stock.controller.base;
+
+public class BaseController {
+
+}
---
+++ marine-stock/src/main/java/com/ocean/stock/controller/stock/StockController.java
@@ -0,0 +1,11 @@
+package com.ocean.stock.controller.stock;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ocean.stock.controller.base.BaseController;
+
+@RequestMapping(value="stock")
+public class StockController extends BaseController{
+
+    
+}
